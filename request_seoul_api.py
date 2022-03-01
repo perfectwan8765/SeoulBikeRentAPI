2cdc0451d49f9cd89bb82255791d2cd7455aa4a6
jsw <perfectwan8765@gmail.com>
2022-03-01 21:24:58 +0900

    UPDATE Make Parquet not CSV

--- request_seoul_api.py
+++ request_seoul_api.py
@@ -7,6 +7,7 @@ import boto3
 from botocore.exceptions import ClientError
 import schedule
 import time
+from fastparquet import write
 
 g_type = 'json'
 g_service = 'bikeList'
@@ -14,9 +15,9 @@ g_api_host = 'http://openapi.seoul.go.kr:8088'
 g_collection_date = datetime.datetime.now()
 
 def make_seoul_rent_bike_info_csv () :
+    global g_collection_date
     log.basicConfig(level=log.ERROR)
 
-    global g_collection_date
     g_collection_date = datetime.datetime.now()
     station_csv_df = update_seoul_rent_bike_station_csv()
 
@@ -86,25 +87,45 @@ def make_csv_for_result_of_api(station_csv_df, response) :
         ['stationId', 'stationNum', 'stationName', 'gu', 'address', 'stationLatitude', 'stationLongitude', 'rackTotCnt', 'parkingBikeTotCnt', 'shared', 'collectionDate']
     ]
 
-    file_name = 'seoul_rentbike_info_{}.csv'.format(g_collection_date.strftime('%m_%d_%Y'))
+    type_map =  {
+        'stationId': 'utf8',
+        'stationNum': 'int32',
+        'stationName': 'utf8',
+        'gu':'utf8',
+        'address':'utf8',
+        'stationLatitude':'utf8',
+        'stationLongitude':'utf8',
+        'rackTotCnt':'int32',
+        'parkingBikeTotCnt':'int32',
+        'shared':'int32'
+    }
 
-    result_df.to_csv(file_name, encoding='UTF-8', index=False)
+    #file_name = 'seoul_rentbike_info_{}.csv'.format(g_collection_date.strftime('%m_%d_%Y'))
+    file_name = 'seoul_rentbike_info_{}.parquet'.format(g_collection_date.strftime('%m_%d_%Y'))
+
+    #result_df.to_csv(file_name, encoding='UTF-8', index=False)
+    #result_df.to_parquet(file_name)
+    write(file_name, result_df, compression='GZIP', times='int96', object_encoding=type_map)
 
     if len(no_address_list) != 0 :
         with open('no_address_list.txt', 'w', encoding='UTF-8') as outfile:
             for station in no_address_list :
                 outfile.write(station + '\n')
 
-    # upload_file(file_name, 'seoulrentbike')
+    upload_file(file_name, 'seoulrentbike')
 
 def update_station_dict (dict) :
     global g_collection_date
     station_name = dict['stationName']
     station_name_split = station_name.strip('"').split('.')
 
-    dict['stationName'] = f'{station_name_split[0]}.{station_name_split[1].lstrip()}'
-    dict['stationNum'] = int(station_name_split[0])
-    dict['collectionDate'] = g_collection_date.strftime('%m/%d/%Y')
+    if len(station_name_split) > 1 :
+        dict['stationName'] = f'{station_name_split[0]}.{station_name_split[1].lstrip()}'
+        dict['stationNum'] = int(station_name_split[0])
+    else :
+        dict['stationNum'] = 0
+
+    dict['collectionDate'] = g_collection_date
 
 def upload_file(file_name, bucket, object_name=None):
     global g_collection_date
@@ -120,10 +141,8 @@ def upload_file(file_name, bucket, object_name=None):
         log.error(e)
 
 
-# schedule.every().day.at('08:25').do(make_seoul_rent_bike_info_csv)
+schedule.every().day.at('02:00').do(make_seoul_rent_bike_info_csv)
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
-make_seoul_rent_bike_info_csv()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
